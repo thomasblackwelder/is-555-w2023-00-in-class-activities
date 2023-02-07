3d0de973c33c3d99696628dde941a3d97bde53bb
davidwilson-nid <davidwilsonphd@gmail.com>
2023-02-07 12:27:20 -0700

    more additions

--- 6 visualization day 1/section 1.R
+++ 6 visualization day 1/section 1.R
@@ -40,6 +40,22 @@
 #   color <- defines the color of the elements produced by that geom
 #   alpha <- defines the opacity (or "see-through-ness") of the elements produced by that geom
 #   
+# FACETS
+#   Facets allow you to use categories within your data to line up similar
+#   charts next to each other, which can be a really useful way to highlight
+#   differences between groups.
+# 
+# The two main facet functions are:
+#   facet_wrap() <- usually best used to show a series of plots across a 
+#                  single category variable, with you specify with `~varable`.
+#                  This will show one plot per level of the category.
+#   facet_grid() <- My favorite to show a "grid" of facets, with rows and 
+#                  columns defined by two different variables. This is specified
+#                  using the facet formula `var1~var2`
+#       Note: Pay attention to the `scales` parameter in these facet functions, 
+#             where you can specify whether you want to hold the x and/or y
+#             axis scales constant across all of the facets.
+# 
 
 library(tidyverse)
 
