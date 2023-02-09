96f45bf6753b5dd1c32fe8c8637c97a867aaeea8
davidwilson-nid <davidwilsonphd@gmail.com>
2023-02-09 12:32:09 -0700

    Create exploration hands on.R

---
+++ 7 DS Workflows Hands On/exploration hands on.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+df1 <- read_csv('https://www.dropbox.com/s/df2w04r0c09kw3f/df_1.csv?dl=1')
+df2 <- read_csv('https://www.dropbox.com/s/xfp1qzxvo19ym0x/df_2.csv?dl=1')
+df3 <- read_csv('https://www.dropbox.com/s/uzusr9723ffn546/df_3.csv?dl=1')
+df4 <- read_csv('https://www.dropbox.com/s/js8tehtsk7btpeq/df_4.csv?dl=1')
+
