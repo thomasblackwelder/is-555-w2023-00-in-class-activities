7378c7e33ea91e84eb35a6a7e86dbced65a2c14f
davidwilson-nid <davidwilsonphd@gmail.com>
2023-02-14 01:03:07 -0700

    update

--- 7 DS Workflows Hands On/exploration hands on.R
+++ 7 DS Workflows Hands On/exploration hands on.R
@@ -5,3 +5,29 @@ df2 <- read_csv('https://www.dropbox.com/s/xfp1qzxvo19ym0x/df_2.csv?dl=1')
 df3 <- read_csv('https://www.dropbox.com/s/uzusr9723ffn546/df_3.csv?dl=1')
 df4 <- read_csv('https://www.dropbox.com/s/js8tehtsk7btpeq/df_4.csv?dl=1')
 
+df1 %>% summarize_all(list(mean=mean, sd=sd))
+df2 %>% summarize_all(list(mean=mean, sd=sd))
+df3 %>% summarize_all(list(mean=mean, sd=sd))
+df4 %>% summarize_all(list(mean=mean, sd=sd))
+
+df1 %>% summary() 
+df2 %>% summary() 
+df3 %>% summary() 
+df4 %>% summary() 
+
+df1 %>% cor()
+df2 %>% cor()
+df3 %>% cor()
+df4 %>% cor()
+
+
+
+
+
+df1 %>% summarize_all(list(mean=mean, sd=sd))
+
+# correlation matrix:
+df1 %>% cor()
+
+# simple regression analysis:
+lm(y ~ x, data = df1) %>% coefficients()
---
+++ 8 algorithms/algorithms.R
@@ -0,0 +1,3 @@
+library(tidyverse)
+
+algs <- read_csv('https://www.dropbox.com/s/qomjauiqxb8o4qd/kaggle_algoriths.csv?dl=1')
